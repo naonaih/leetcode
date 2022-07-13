31b58b064435e657911d04b8cac8288dbe89d58c
Naoya Hara <naonaih@gmail.com>
2022-07-13 09:27:10 +0900

    Added 142_linked_list_cycle2.py

---
+++ problems/142_linked_list_cycle2.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        nodes_seen = set()
+        while head is not None:
+            if head in nodes_seen:
+                return head
+            nodes_seen.add(head)
+            head = head.next
+        return None
+
