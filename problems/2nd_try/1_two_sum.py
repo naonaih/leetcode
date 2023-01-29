76d6f717f67eadd917ac79e1d0008981b9605c66
Naoya Hara <naonaih@gmail.com>
2023-01-29 10:12:14 +0900

    Added 1_two_sum.py for 2nd try

--- problems/2nd_try/1_two_sum.py
+++ problems/2nd_try/1_two_sum.py
@@ -1,10 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         c = Counter(nums)
-        print(c)
         for i in range(len(nums)):
             diff = target - nums[i]
-            print(diff)
             if diff in c:
                 j = nums.index(diff)
                 if i == j:
