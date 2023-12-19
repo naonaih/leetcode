472be75fca9ae58bd075548716abe6acae279520
Naoya Hara <naonaih@gmail.com>
2023-12-19 23:04:36 +0900

    Added 102_binary_tree_level_order_traversal.py for 2nd try

---
+++ problems/2nd_try/102_binary_tree_level_order_traversal.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        from collections import defaultdict
+        d = defaultdict(list)
+        def dfs(node, level):
+            if node is None:
+                return
+            d[level].append(node.val)
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+
+        dfs(root, 0)
+        return list(d.values())
